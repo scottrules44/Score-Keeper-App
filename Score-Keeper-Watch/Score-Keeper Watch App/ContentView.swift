113b26c0d171a5f838f9724ebe9ce66b8d646bc3
Scott Harrison <scottrules44@gmail.com>
2023-10-04 10:56:21 -0500

    Watch OS Fixes

--- Score-Keeper-Watch/Score-Keeper Watch App/ContentView.swift
+++ Score-Keeper-Watch/Score-Keeper Watch App/ContentView.swift
@@ -113,7 +113,7 @@ struct ContentView: View {
    var body: some View {
        HStack {
            VStack {
-               Text("Home \(Int(player1))")
+               Text("Home \(Int(player1))").foregroundColor(Color(hex:"#FF10F0"))
                Button(action: {
                    sendPutRequest(playerNum: 1, mode:AddOrSub.ADD)
                }) {
@@ -122,7 +122,7 @@ struct ContentView: View {
                }
                .padding()
                Button(action: {
-                   sendPutRequest(playerNum: 3, mode:AddOrSub.SUB)
+                   sendPutRequest(playerNum: 1, mode:AddOrSub.SUB)
                }) {
                    Image(systemName: "minus.circle")
                        .font(.largeTitle)
@@ -136,7 +136,7 @@ struct ContentView: View {
                    showAlert = true
                }) {
                    Image(systemName: "arrow.clockwise.circle")
-                       .font(.largeTitle)
+                       .font(.title2).padding(0)
                }.alert(isPresented: $showAlert) {
                    Alert(
                        title: Text("Score Reset"),
@@ -146,12 +146,12 @@ struct ContentView: View {
                        },
                        secondaryButton: .cancel()
                    )
-               }
+               }.buttonStyle(PlainButtonStyle())
 
                
            }
            VStack {
-               Text("Away \(Int(player2))")
+               Text("Away \(Int(player2))").foregroundColor(Color(hex:"#00FFFF"))
                Button(action: {
                    sendPutRequest(playerNum: 2, mode:AddOrSub.ADD)
                }) {
@@ -176,5 +176,33 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
 }
